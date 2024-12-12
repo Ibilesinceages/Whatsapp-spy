ff62096b5c6ef892eedbed231dd483190704a561
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-12-12 08:38:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,36 @@
-clear
-sudo apt update
-sudo apt install python3 nodejs npm 
+#!/bin/bash
+
+# Check if running on Termux
+if [[ -n "$TERMUX_VERSION" ]]; then
+        clear
+        pkg update
+        pkg install python3 nodejs npm 
+        pip install rich 
+        npm install 
+        python3 main.py
+elif [[ -f "/data/data/com.termux/files/usr/bin/bash" ]]; then
+        clear
+        pkg update
+        pkg install python3 nodejs npm 
+        pip install rich 
+        npm install 
+        python3 main.py
+else
+    # Check if running on Linux
+    if [[ "$(uname -o)" == "GNU/Linux" ]]; then
+        clear
+        sudo apt update
+        sudo apt install python3 nodejs npm 
+        pip install rich 
+        npm install 
+        python3 main.py
+
+    else
+        echo "Unknown environment."
+    fi
+fi
 
 
-pip install rich 
-npm install 
-python3 main.py
+
+
+
