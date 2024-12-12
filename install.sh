7e969e1f30e20ad0f50f38d1bcdd30d047d3a008
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-12-12 08:39:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Check if running on Termux
+
 if [[ -n "$TERMUX_VERSION" ]]; then
         clear
         pkg update
-        pkg install python3 nodejs npm 
+        pkg install python3 nodejs npm -y
         pip install rich 
         npm install 
         python3 main.py
 elif [[ -f "/data/data/com.termux/files/usr/bin/bash" ]]; then
         clear
         pkg update
-        pkg install python3 nodejs npm 
+        pkg install python3 nodejs npm -y
         pip install rich 
         npm install 
         python3 main.py
 else
-    # Check if running on Linux
+   
     if [[ "$(uname -o)" == "GNU/Linux" ]]; then
         clear
         sudo apt update
-        sudo apt install python3 nodejs npm 
+        sudo apt install python3 nodejs npm -y
         pip install rich 
         npm install 
         python3 main.py
